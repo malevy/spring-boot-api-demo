021d36f3da3affa51f686d85018cf9eeb53f042a
Michael Levy <malevy@outlook.com>
2017-11-06 22:28:17 -0500

    rough in controller in order to see how it looks

---
+++ src/main/java/net/malevy/hyperdemo/TaskController.java
@@ -0,0 +1,56 @@
+package net.malevy.hyperdemo;
+
+import net.malevy.hyperdemo.commands.CommandFactory;
+import net.malevy.hyperdemo.commands.GetSingleTaskCommand;
+import net.malevy.hyperdemo.support.HttpProblem;
+import net.malevy.hyperdemo.support.westl.Wstl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/tasks")
+public class TaskController {
+
+    private final CommandFactory commandFactory;
+
+    @Autowired
+    public TaskController(CommandFactory commandFactory) {
+        this.commandFactory = commandFactory;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> getTask(@PathVariable() Integer id) {
+
+        GetSingleTaskCommand command = this.commandFactory.getCommand(GetSingleTaskCommand.class);
+
+        Optional<Wstl> wstl = command.execute(id)
+                .map(WstlMapper::FromTask);
+
+        return wstl.isPresent()
+                ? ok(wstl.get())
+                : notFound(id);
+
+    }
+
+    private ResponseEntity<Wstl> ok(Wstl wstl) {
+        return new ResponseEntity<>(wstl, HttpStatus.OK);
+    }
+
+    private ResponseEntity<HttpProblem> notFound(Integer id) {
+
+        HttpProblem problem = HttpProblem.builder()
+                .title(String.format("Task with id {%s} not found", id))
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+
+        return new ResponseEntity<>(problem, HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ src/main/java/net/malevy/hyperdemo/WstlMapper.java
@@ -0,0 +1,13 @@
+package net.malevy.hyperdemo;
+
+import net.malevy.hyperdemo.models.domain.Task;
+import net.malevy.hyperdemo.support.westl.Wstl;
+
+public class WstlMapper {
+
+    public static Wstl FromTask(Task t) {
+
+        return null;
+    }
+
+}
